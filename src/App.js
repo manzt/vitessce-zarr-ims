a1f7956f4d5825c33d1e8e9ddc625a09df121e4d
Trevor Manz <trevmanz94@gmail.com>
2019-12-14 16:22:28 -0500

    heatmap working; TODO use generator for indexing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { openArray } from "zarr";
+import Heatmap from "./components/Heatmap";
 
 const config = {
   store: "http://localhost:3000/", // proxy on localhost:8000
@@ -9,24 +10,33 @@ const config = {
 };
 
 function App() {
-  const [imsData, setImsData] = useState([]);
+  const [imsData, setImsData] = useState(0);
+  const [data2d, setData2d] = useState(0);
 
-  async function getSlice(x, y, z) {
-    const { arr } = imsData;
+  async function getSlice(arr, x, y, z) {
     const slice = await arr.get([x, y, z]);
-    console.log(slice);
+    return slice;
   }
 
   useEffect(() => {
     async function bindArray() {
       const z = await openArray(config);
       const meta = await fetch("VAN_meta.json").then(res => res.json());
-      setImsData({ arr: z, meta: meta });
+      const data = { arr: z, meta: meta };
+      const dataSlice = await getSlice(z, 0, null, null);
+      setImsData(data);
+      setData2d(dataSlice.data);
     }
     bindArray();
   }, []);
 
-  return <div className="App">yo</div>;
+  return (
+    <div className="App">
+      {imsData !== 0 && data2d !== 0 ? (
+        <Heatmap data2d={data2d} meta={imsData.meta} />
+      ) : null}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -5,10 +5,11 @@ import {
   OrthographicView,
   COORDINATE_SYSTEM
 } from "deck.gl";
+import { cross, range } from "d3-array";
 
-const INITIAL_VIEW_STATE = { target: [0, 0, 0], zoom: -5 };
+const INITIAL_VIEW_STATE = { target: [1317.0, 901.5, 0], zoom: -2 };
 
-const Heatmap = ({ data }) => {
+const Heatmap = ({ data2d, meta }) => {
   /**
    * Data format:
    * [
@@ -16,11 +17,13 @@ const Heatmap = ({ data }) => {
    *   ...
    * ]
    */
+  const indexes = cross(range(meta.x.length), range(meta.y.length));
+  console.log(indexes);
   const layer = new HeatmapLayer({
-    data: data,
+    data: indexes,
     id: "heatmapLayer",
-    getPosition: d => d.COORDINATES,
-    getWeight: d => d.WEIGHT,
+    getPosition: d => [meta.x[d[0]], meta.y[d[1]]],
+    getWeight: d => data2d[d[0]][d[1]],
     coordinateSystem: COORDINATE_SYSTEM.IDENTITY
   });
 
